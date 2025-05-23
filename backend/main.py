3e7859eac4417530a3a241a6ca04c85cf527cdd7
kartikey1188 <kartikeysingh428@gmail.com>
2025-05-23 20:13:44 +0530

    documents added

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
+from pathlib import Path
 
 from backend.app.api import *
 
 load_dotenv()
 
 api = FastAPI()
+
+# Calculating base directory and mounting static files from the frontend folder.
+BASE_DIR = Path(__file__).resolve().parent.parent
+api.mount("/static", StaticFiles(directory=str(BASE_DIR / "frontend")), name="static")
 
 api.include_router(index_router)
 
